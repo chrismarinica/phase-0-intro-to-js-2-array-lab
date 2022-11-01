a507f93d2f5a3d38f3266dcb3cabd1f7759ba44c
Christopher Marinica <chris.marinica@gmail.com>
2022-11-01 12:26:18 -0400

    commit tree 52a554fb14898f8973d129ee8f2109ef3dbbe2ca

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield']
+
+function destructivelyAppendCat(){
+    return cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    return cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift('Milo');
+}
+
+function appendCat(){
+    const copyOfCats = [...cats, 'Broom'];
+    return copyOfCats;
+}
+
+function prependCat(){
+    const copyOfCats = ['Arnold', ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat(){
+    const catList = cats.slice(0,cats.length-1);
+    return catList;
+}
+
+function removeFirstCat(){
+    const catsList = cats.slice(1)
+    return catsList;
+}
